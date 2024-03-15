b318e0bc1c30678b2213819236f6c2e7899fb9a2
folka2134 <folka2134@gmail.com>
2024-03-15 16:04:32 +0000

    created footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export default function RootLayout({
@@ -9,7 +10,7 @@ export default function RootLayout({
     <div className="flex h-screen flex-col">
       <Navbar />
       <main className="flex-1">{children}</main>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="bg-gray-800 p-4 text-center text-white">
+      <div>
+        <p>&copy; 2021 ReciNow</p>
+      </div>
+    </footer>
+  );
+};
