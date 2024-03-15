8dadb8196e00aa3581efa25156393f8353cfdbdf
folka2134 <folka2134@gmail.com>
2024-03-15 16:00:01 +0000

    create recipe button

--- components/shared/RecipeCard.tsx
+++ components/shared/RecipeCard.tsx
@@ -1,10 +1,42 @@
 import { Recipe } from "@/types/recipe";
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { Button } from "../ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
 type RecipeCardProps = {
   recipe: Recipe;
 };
 
 export const RecipeCard = ({ recipe }: RecipeCardProps) => {
-  return <div>RecipeCard</div>;
+  return (
+    <Card className="">
+      <Link href={`/recipe/${recipe.id}`}>
+        <CardHeader className="p-2">
+          <Image
+            className="stretch"
+            src={recipe.images[0]}
+            alt={recipe.title}
+            width={200}
+            height={200}
+          />
+        </CardHeader>
+        <CardContent className="px-3 py-1">
+          <CardTitle className="text-md">{recipe.title}</CardTitle>
+        </CardContent>
+      </Link>
+      <CardFooter className="flex justify-between p-2">
+        <Button variant="outline">Like</Button>
+        <Button variant="outline">Save</Button>
+      </CardFooter>
+    </Card>
+  );
 };
